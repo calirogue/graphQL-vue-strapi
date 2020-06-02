6a9421488bb81514a199b4d6e53a01431009e7b3
bellagrunt <mattbellantoni3@gmail.com>
2020-06-02 13:51:19 -0400

    setting up front and backend in apollo

---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f92373e3-dc01-450a-950d-25125a09c50e'
+};
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,9 +3,19 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 
+import VueApollo from "vue-apollo";
+import apolloClient from "./vue-apollo";
+
 Vue.config.productionTip = false;
+
+Vue.use(VueApollo);
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient
+});
 
 new Vue({
   router,
+  apolloProvider,
   render: h => h(App)
 }).$mount("#app");
